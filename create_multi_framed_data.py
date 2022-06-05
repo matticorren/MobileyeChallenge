a4606960d69f6e2b669958f766284a60fec8d4ae
Matti Corren <matti.corren@gmail.com>
2022-06-06 01:08:49 +0300

    adding tests

--- create_multi_framed_data.py
+++ create_multi_framed_data.py
@@ -8,22 +8,29 @@ INPUT = ".\\training_data"
 OUTPUT = ".\\output"
 
 
-def create_multi_framed_data(input=INPUT, output=OUTPUT):
-    hand = Handler(input, output)
-    hand.reorganize_dataset()
+def create_multi_framed_data(input_dir=INPUT, output_dir=OUTPUT):
+    """
+    This function reorganizes the data sets as multi-framed
+    :param input: the dir containing the original data sets.
+    :param output: the dir to save the new data sets.
+    """
+    handler = Handler(input_dir, output_dir)
+    handler.reorganize_dataset()
 
 
 def main():
-    create_multi_framed_data()
-    # parser = argparse.ArgumentParser(description="Process arguments.")
-    # parser.add_argument("--single_frame_dir",
-    #                     dest="input",
-    #                     help="Single-Frame dir's path.")
-    # parser.add_argument("--output_dir",
-    #                     dest="output",
-    #                     help="A dir to save in it the output.")
-    # args = parser.parse_args()
-    # create_multi_framed_data(args.input, args.output)
+    """
+    main function
+    """
+    parser = argparse.ArgumentParser(description="Process arguments.")
+    parser.add_argument("--single_frame_dir",
+                        dest="input",
+                        help="Single-Frame dir's path.")
+    parser.add_argument("--output_dir",
+                        dest="output",
+                        help="A dir to save in it the output.")
+    args = parser.parse_args()
+    create_multi_framed_data(args.input, args.output)
 
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -3,32 +3,52 @@ import pytest
 
 # Internal imports
 from plot_image_with_labels import plot_image_with_labels
+from handler import Handler
+
+# Constants
+INPUT = ".\\training_data"
+OUTPUT = ".\\output"
+TEST_SINGLE = ".\\tests\\single"
+TEST_MULTIPLE = ".\\tests\\single"
+
+handler1 = Handler(input_dir=INPUT, output_dir=TEST_SINGLE)
+handler2 = Handler(input_dir=OUTPUT, output_dir=TEST_MULTIPLE)
 
 
-def validate_extraction_single_frame(index):
+@pytest.mark.parametrize("index", "handler", [121, handler1], [151, handler1], [188, handler1])
+def validate_extraction_single_frame(index, handler):
     """
-    This function validates whether the extraction of the data was successful
+    This function validates whether the extraction of the data was successful.
     :param index: The index of the frame to be shown.
+    :param handler: the Handler object.
     """
-    plot_image_with_labels(image=self.images[index].reshape(80, 80),
-                           sticks=self.sticks[index].reshape(9),
-                           sample_id=self.ids[index].reshape(1)[0],
-                           sample_frame=self.frames[index].reshape(1)[0],
-                           ratios=self.ratios[index].reshape(4))
+    plot_image_with_labels(image=handler.images[index].reshape(80, 80),
+                           sticks=handler.sticks[index].reshape(9),
+                           sample_id=handler.ids[index].reshape(1)[0],
+                           sample_frame=handler.frames[index].reshape(1)[0],
+                           ratios=handler.ratios[index].reshape(4))
 
 
-def validate_extraction_both_frames(images, sticks, frames, ratios, index):
+@pytest.mark.parametrize("index", "handler", [120, handler2], [151, handler2], [188, handler2])
+def validate_extraction_both_frames(index, handler):
     """
-    This function validates whether the extraction of the data was successful
+    This function validates whether the manipulation of the data was successful.
     :param index: The index of the frame to be shown.
+    :param handler: the Handler object.
     """
-    plot_image_with_labels(image=images[index][:, :, 0].reshape(80, 80),
-                           sticks=sticks[index][:, :, 0].reshape(9),
-                           sample_id=self.ids[index].reshape(1)[0],
-                           sample_frame=frames[index][:, :, 0].reshape(1)[0],
-                           ratios=ratios[index][:, :, 0].reshape(4))
-    plot_image_with_labels(image=images[index][:, :, 1].reshape(80, 80),
-                           sticks=sticks[index][:, :, 1].reshape(9),
-                           sample_id=self.ids[index].reshape(1)[0],
-                           sample_frame=frames[index][:, :, 1].reshape(1)[0],
-                           ratios=ratios[index][:, :, 1].reshape(4))
+    for i in range(2):
+        plot_image_with_labels(image=handler.images[index][:, :, i].reshape(80, 80),
+                               sticks=handler.sticks[index][:, :, i].reshape(9),
+                               sample_id=handler.ids[index].reshape(1)[0],
+                               sample_frame=handler.frames[index][:, :, i].reshape(1)[0],
+                               ratios=handler.ratios[index][:, :, i].reshape(4))
+
+
+def test():
+    validate_extraction_single_frame(120, handler1)
+    validate_extraction_both_frames(151, handler2)
+    validate_extraction_both_frames(188, handler2)
+
+
+if __name__ == "__main__":
+    test()

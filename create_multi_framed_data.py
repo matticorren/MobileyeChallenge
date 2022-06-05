62fc1f5d5f83ee58e6e7d8dd066b37ea6fa2085a
Matti Corren <matti.corren@gmail.com>
2022-06-05 17:41:39 +0300

    full extraction implementation

--- create_multi_framed_data.py
+++ create_multi_framed_data.py
@@ -2,21 +2,28 @@
 import numpy as np
 import argparse
 
+# Internal imports
+from handler import Handler
 
-def create_multi_framed_data(input, output):
-    pass
+INPUT = ".\\training_data"
+OUTPUT = ".\\"
+
+def create_multi_framed_data(input=INPUT, output=OUTPUT):
+    hand = Handler(input, output)
+    hand.validate_extraction(360)
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Process arguments.")
-    parser.add_argument("--single_frame_dir",
-                        dest="input",
-                        help="Single-Frame dir's path.")
-    parser.add_argument("--output_dir",
-                        dest="output",
-                        help="A dir to save in it the output.")
-    args = parser.parse_args()
-    create_multi_framed_data(args.input, args.output)
+    create_multi_framed_data()
+    # parser = argparse.ArgumentParser(description="Process arguments.")
+    # parser.add_argument("--single_frame_dir",
+    #                     dest="input",
+    #                     help="Single-Frame dir's path.")
+    # parser.add_argument("--output_dir",
+    #                     dest="output",
+    #                     help="A dir to save in it the output.")
+    # args = parser.parse_args()
+    # create_multi_framed_data(args.input, args.output)
 
 
 if __name__ == "__main__":
--- handler.py
+++ handler.py
@@ -4,6 +4,9 @@ import json
 import numpy as np
 from pathlib import Path
 
+# Internal imports
+from plot_image_with_labels import plot_image_with_labels
+
 
 class Handler(object):
     """
@@ -15,7 +18,10 @@ class Handler(object):
         :param input_dir: The dir of the training data
         :param output_dir: The dir to put the output in.
         """
-        self.output_dir = output_dir
+        if os.path.exists(output_dir):
+            self.output_dir = output_dir
+        else:
+            raise InvalidPath(output_dir)
         if os.path.exists(input_dir):
             self.files = {Path(f).stem: os.path.join(input_dir, f) for f in os.listdir(input_dir)}
         else:
@@ -27,21 +33,41 @@ class Handler(object):
         self.sticks = self._extract_bin("sticks")
         self.ratios = self._extract_bin("ratios")
         self.images = self._extract_bin("images")
+        print(self.images)
 
     def _extract_bin(self, attribute):
         """
-
-        :param attribute:
-        :return:
+        This function extracts the data of a relevant bin file
+        :param attribute: the attribute's bin to be extracted
+        :return: a 4D numpy Array.
         """
-        return np.fromfile(self.files[attribute], self.data_struct[attribute]["type"])
+        attr = self.data_struct[attribute]
+        base = np.fromfile(self.files[attribute], attr["type"])
+        h, w, c = attr["height"], attr["width"], attr["channels"]
+        return base.reshape(base.size//(h*w), h, w, c)
+
+    def validate_extraction(self, index):
+        """
+        This function validates whether the extraction of the data was successful
+        :param index: The index of the frame to be shown.
+        """
+        plot_image_with_labels(image=self.images[index].reshape(80, 80),
+                               sticks=self.sticks[index].reshape(9),
+                               sample_id=self.ids[index].reshape(1)[0],
+                               sample_frame=self.frames[index].reshape(1)[0],
+                               ratios=self.ratios[index].reshape(4))
 
 
 class InvalidPath(Exception):
     """
-
+    This class implements an error occuring when the user inputs a problematic path.
     """
     def __init__(self, path, message="The path {0} doesn't exist. Be sure to enter a valid one."):
+        """
+        This class initialize the InvalidPath Error object.
+        :param path: The problematic path given by the user.
+        :param message: The message to be shown in the console.
+        """
         self.path = path
         self.message = message.format(self.path)
         super().__init__(self.message)
